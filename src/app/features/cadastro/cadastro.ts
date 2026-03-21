d2c489fd727c4c43d0cc3bed3181d792c631e96f
VicBenfica <vick5.5@hotmail.com>
2026-03-21 19:36:35 -0300

    campos

--- src/app/features/cadastro/cadastro.ts
+++ src/app/features/cadastro/cadastro.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { CadastroService } from './service/login.service';
+import { cadastroUsuario } from '../../model/cadastroUsuario';
+import { CustomButton } from '../../shared/components/custom-button/custom-button';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [FormsModule, NgForm, CommonModule, Router, CustomButton],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
-export class Cadastro {}
+export class Cadastro {
+  private router = inject(Router);
+  private loginService = inject(CadastroService)
+
+  userLogin: cadastroUsuario = {
+    nome: '',
+    email: '',
+    senha: ''
+  };
+
+  enviando = signal(false);
+  mensagem = signal('');
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      this.mensagem.set("Preencha todos os campos.");
+      return;
+    }
+
+    this.enviando.set(true);
+
+    setTimeout(() => {
+      this.mensagem.set('Login simulado com sucesso!');
+      this.enviando.set(false);
+      this.router.navigate(['/']);
+    }, 1000);
+  }
+}
+
---
+++ src/app/model/cadastroUsuario.ts
@@ -0,0 +1,6 @@
+
+export interface cadastroUsuario {
+    nome: string;
+    email: string;
+    senha: string;
+}
