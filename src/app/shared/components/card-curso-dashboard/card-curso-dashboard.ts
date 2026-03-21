f7cf78c900e1f3c7f38f08d79cd3620a977926b8
serrabruna <brunaibserra@hotmail.com>
2026-03-21 16:37:49 -0300

    feat: componente card-curso-dashboard com progresso dinamico

---
+++ src/app/shared/components/card-curso-dashboard/card-curso-dashboard.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomButton } from '../custom-button/custom-button';
+
+@Component({
+  selector: 'app-card-curso-dashboard',
+  standalone: true,
+  imports: [CommonModule, CustomButton],
+  templateUrl: './card-curso-dashboard.html',
+  styleUrl: './card-curso-dashboard.css',
+})
+export class CardCursoDashboard {
+  @Input() imageUrl: string = 'assets/imageScratch.png';
+  @Input() description: string = 'Lorem ipsum dolor sit amet, consectetur.';
+  @Input() progress: number = 75;
+
+  getProgressStyle() {
+    return {
+      width: `${this.progress}%`
+    };
+  }
+}
