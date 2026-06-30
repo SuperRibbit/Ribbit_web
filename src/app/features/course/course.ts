540ffb263027db3e213e0a3f37d58250d413bc5f
serrabruna <brunaibserra@hotmail.com>
2026-06-30 20:43:34 -0300

    integrando api em pagina course

--- src/app/features/course/course.ts
+++ src/app/features/course/course.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Modules } from "../../shared/components/modules/modules";
+import { CourseService } from '../../services/course.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-course',
@@ -7,25 +9,29 @@ import { Modules } from "../../shared/components/modules/modules";
   templateUrl: './course.html',
   styleUrl: './course.css',
 })
-export class Course {
-  public curso: any = {
-  title: 'Nome do Curso',
-  modules: [
-    { 
-      module_id: 1, 
-      title: 'Introdução ao Scratch',
-      classes: [
-        { class_id: 101, title: 'O que é Scratch?', is_completed: true },
-        { class_id: 102, title: 'Interface do Usuário', is_completed: false }
-      ]
-    },
-    { 
-      module_id: 2, 
-      title: 'Configurando o Ambiente',
-      classes: [
-        { class_id: 201, title: 'Instalando Extensões', is_completed: false }
-      ]
+export class Course implements OnInit{
+  private route = inject(ActivatedRoute);
+  private courseService = inject(CourseService);
+
+  
+  public curso = signal<any>(null);
+
+  ngOnInit() {
+    const courseId = this.route.snapshot.paramMap.get('id');
+    if (courseId) {
+      this.loadCourseDetails(Number(courseId));
     }
-  ]
-};
+  }
+
+  private loadCourseDetails(id: number) {
+    this.courseService.getCourseById(id).subscribe({
+      next: (response) => {
+        const dadosCurso = response.course ? response.course : response;
+        this.curso.set(dadosCurso);
+      },
+      error: (err) => {
+        console.error('Erro ao buscar os detalhes do curso:', err);
+      }
+    });
+  }
 }
