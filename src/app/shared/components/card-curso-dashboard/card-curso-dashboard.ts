e474037ac7e016f55d763ff27c3b385d3e85bd74
serrabruna <brunaibserra@hotmail.com>
2026-09-02 20:34:35 -0300

    feat(): implementar navegação no botão de retomar curso

--- src/app/shared/components/card-curso-dashboard/card-curso-dashboard.ts
+++ src/app/shared/components/card-curso-dashboard/card-curso-dashboard.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomButton } from '../custom-button/custom-button';
 import { DriveImgPipe } from "../../../utilities/pipes/drive-img-pipe";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card-curso-dashboard',
@@ -15,4 +16,13 @@ export class CardCursoDashboard{
   @Input() imageUrl: string = 'assets/imageScratch.png';
   @Input() description: string = 'Lorem ipsum dolor sit amet, consectetur.';
   @Input() progress: number = 0;
+  @Input() courseId!: number | string;
+
+  private router = inject(Router);
+
+  retomarCurso(): void {
+    if (this.courseId) {
+      this.router.navigate(['/player', this.courseId]);
+    }
+  }
 }
